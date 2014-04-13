eed22446e8a2527e4e9c78cf0d071d2347b444c7
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-04-13 01:43:10 -0400

    auto generate that stuff

---
+++ field_of_view/create_from_text_file/FileComprehension.py
@@ -0,0 +1,230 @@
+import xmldict # pip install xmldict
+import json # built-in
+import types # built-in
+
+class TexDefaultOptions(object):
+    defaultOptions = {'general' : {
+        'indent' : 0,
+        'indentStep' : 4,
+        'tikzpicture' : {},
+        'scale' : '1.0\\linewidth',
+        'packages' : [('','tikz')],
+        'tikzlibraries' : 'fit,calc,positioning,backgrounds',
+        'layers' : 'background,lower,upper,main'
+    },
+    'completeScope' : {
+        'layer' : 'lower',
+        'scope' : {
+            'options' : {
+                'opacity' : 1.0
+            }
+        },
+        'images' : {
+            'example_images/arnie.jpg' : {
+                'index' : 1, 'options' : {}
+            },
+            'example_images/arnie2.jpg' : {
+                'index' : 2, 'options' : {}
+            }
+        },
+        'nodeNameBase' : 'completeView',
+        'options' : {},
+    },
+    'fieldOfViewScope' : {
+        'options' : {},
+        'scope' : {
+            'options' : {
+                'opacity' : 1.0,
+                'yshift' : '-2cm'
+            }
+        },
+        'lines' : {
+            1 : {
+                'options' : {},
+                'images' : {
+                    'example_images/arnie.jpg' : {
+                        'index' : 1, 'options' : {}
+                    },
+                    'example_images/arnie2.jpg' : {
+                        'index' : 2, 'options' : {}
+                    },
+                    'example_images/arnie3.jpg' : {
+                        'index' : 3, 'options' : {}
+                    },
+                    'example_images/arnie4.jpg' : {
+                        'index' : 4, 'options' : {}
+                    }
+                }
+            }
+        },
+        'fakeNameBase' : 'fieldOfView',
+        'nodeNameBase' : 'fieldOfView',
+    },
+    'overlay' : {
+        'scope' : {
+            'options' : {
+                'overlay' : None
+            }
+        },
+        'options' : {
+            'opacity' : 0.3,
+            'color' : 'red!30'
+        },
+        'pairs' : {
+            (1,1) : { 'frameOptions' : {}, 'connectorOptions' : {}},
+            (2,4) : { 'frameOptions' : {}, 'connectorOptions' : {}}
+        },
+        'bend' : 0,
+        'indicatorNameBase' : 'indicator',
+        'frameNameBase' : 'frame'
+    }
+    }
+        
+
+    def __init__(self):
+        pass
+
+    def mergeUserOptions(self, userOptions):
+        mergedDict = dict(TexDefaultOptions.defaultOptions)
+        TexDefaultOptions.merge(userOptions, mergedDict)
+        return mergedDict
+
+    @staticmethod
+    def merge(first, second):
+        for key, value in first.iteritems():
+            if type(value) is types.DictType:
+                if not second.has_key(key):
+                    second[key] = dict(value)
+                else:
+                    TexDefaultOptions.merge(value, second[key])
+            else:
+                second[key] = value
+                
+
+
+class ToDictConverter(object):
+    def convert(self):
+        return TexDefaultOptions.defaultOptions
+
+
+class JsonToDictConverter(ToDictConverter):
+    def __init__(self, filename):
+        super(JsonToDictConverter, this).__init__()
+        self.filename = filename
+
+    def convert(self):
+        return json.load(open(self.filename))
+
+
+class XmlToDictConverter(ToDictConverter):
+    def __init__(self, filename):
+        super(JsonToDictConverter, this).__init__()
+        self.filename = filename
+
+    def convert(self):
+        return xmldict.dict_to_xml(open(self.filename))
+
+
+class OptionParser(object):
+    @staticmethod
+    def parse(options):
+        OptionParser.addGlobalOptionsToLocal(options['general'], options['completeScope'])
+        OptionParser.addGlobalOptionsToLocal(options['general'], options['fieldOfViewScope'])
+        OptionParser.addGlobalOptionsToLocal(options['general'], options['overlay'])
+        completeScopeOptions = options['completeScope']
+        OptionParser.parseCompleteScopeOptions(completeScopeOptions)
+        fieldOfViewScopeOptions = options['fieldOfViewScope']
+        OptionParser.parseFieldOfViewScopeOptions(fieldOfViewScopeOptions)
+        overlayOptions = options['overlay']
+        OptionParser.parseOverlayOptions(overlayOptions, completeScopeOptions['images'], fieldOfViewScopeOptions['images'])
+
+        return options
+
+    @staticmethod
+    def parseCompleteScopeOptions(completeScopeOptions):
+        images = completeScopeOptions['images']
+        prevKey = -1
+        for image, options in sorted(images.iteritems(), key=lambda x: int(x[1]['index'])):
+            options['nodeName'] = completeScopeOptions['nodeNameBase'] + str(options['index'])
+            generalImageOptions = dict(completeScopeOptions['options'])
+            TexDefaultOptions.merge(options['options'], generalImageOptions)
+            options['options'] = generalImageOptions
+            if prevKey > -1:
+                options['options']['right'] = 'of %s%d' % (completeScopeOptions['nodeNameBase'], prevKey)
+            prevKey = int(options['index'])
+            
+
+    @staticmethod
+    def parseFieldOfViewScopeOptions(fieldOfViewScopeOptions):
+        keyValuePairs = []
+        for line, lineContent in fieldOfViewScopeOptions['lines'].iteritems():
+            generalLineOptions = dict(fieldOfViewScopeOptions['options'])
+            TexDefaultOptions.merge(lineContent['options'], generalLineOptions)
+            lineContent['options'] = generalLineOptions
+            images = lineContent['images']
+            prevKey = -1
+            for image, options in sorted(images.iteritems(), key=lambda x: int(x[1]['index'])):
+                options['nodeName'] = fieldOfViewScopeOptions['nodeNameBase'] + str(options['index'])
+                generalImageOptions = dict(lineContent['options'])
+                TexDefaultOptions.merge(options['options'], generalImageOptions)
+                options['options'] = generalImageOptions
+                keyValuePairs.append((image, options))
+                if prevKey > -1:
+                    options['options']['right'] = 'of %s%d' % (fieldOfViewScopeOptions['nodeNameBase'], prevKey)
+                prevKey = int(options['index'])
+        fieldOfViewScopeOptions['images'] = {}
+        for key, value in keyValuePairs:
+            fieldOfViewScopeOptions['images'][key] = value
+
+
+    @staticmethod
+    def parseOverlayOptions(overlayOptions, completeScopeImages, fieldOfViewImages):
+        pairs = overlayOptions['pairs']
+        for idx, (pair, options) in enumerate(pairs.iteritems()):
+            generalPairOptions = dict(overlayOptions['options'])
+            TexDefaultOptions.merge(options['frameOptions'], generalPairOptions)
+            options['frameOptions'] = generalPairOptions
+            generalPairOptions = dict(overlayOptions['options'])
+            TexDefaultOptions.merge(options['connectorOptions'], generalPairOptions)
+            options['connectorOptions'] = generalPairOptions
+            def findNodeName(query, nodeDict):
+                if type(query) == int:
+                    res = [x for x in nodeDict.iteritems() if x[1]['index'] == query]
+                elif type(query) == str:
+                    res = [x for x in nodeDict.iteritems() if x[0] == query]
+                else:
+                    raise Exception("I don't understand the type!")
+                if len(res) == 0:
+                    raise Exception("I did not find the key %s!" % str(input))
+                return res[0][1]['nodeName']
+            options['nodeFrom'] = findNodeName(pair[0], completeScopeImages)
+            options['nodeTo']   = findNodeName(pair[1], fieldOfViewImages)
+            options['indicatorName'] = overlayOptions['indicatorNameBase'] + str(idx)
+            options['frameName'] = overlayOptions['frameNameBase'] + str(idx)
+            # calculate relative position in image!
+            # calculate bend!
+
+            
+    @staticmethod
+    def addGlobalOptionsToLocal(globalDict, localDict):
+        options = ['indent', 'indentStep']
+        for option in options:
+            localDict[option] = globalDict[option]
+
+
+                
+                
+        
+        
+
+
+if __name__ == "__main__":
+    texDefaultOptions = TexDefaultOptions()
+    someDict = {1 : 'b', 'a' : {'a' : 2}}
+    print OptionParser.parse(texDefaultOptions.mergeUserOptions(someDict))
+
+
+
+
+        
+
---
+++ field_of_view/create_from_text_file/TexObject.py
@@ -0,0 +1,271 @@
+from collections import defaultdict
+import re
+
+import FileComprehension
+
+class TexHelper(object):
+    @staticmethod
+    def composeOptions(texOptions):
+        optionsString = '['
+        for option, value in texOptions.iteritems():
+            optionsString += str(option)
+            if value is not None:
+                optionsString += '=%s' % str(value)
+            optionsString += ','
+        optionsString += ']'
+        return optionsString.replace(',]', ']')
+
+    @staticmethod
+    def createIndentString(nIndent):
+        return ' ' * int(nIndent)
+
+    @staticmethod
+    def indentEachLineInString(nIndent, string):
+        return '\n'.join(re.sub('^', '%s' % TexHelper.createIndentString(nIndent), x) for x in string.split('\n'))
+
+    @staticmethod
+    def createImageNodeString(filename, nodeIndex, options, basename):
+        options['inner sep'] = 0
+        return '\\node%s (%s%s) {\includegraphics[width=\\textwidth]{%s}};' % (TexHelper.composeOptions(options),
+                                                                               basename,
+                                                                               str(nodeIndex),
+                                                                               str(filename))
+
+    @staticmethod
+    def createLayerString(layerList):
+        resultString = ''
+        for layer in layerList.split(','):
+            if layer == 'background' or layer == 'main':
+                continue
+            resultString += '\\pgfdeclarelayer{%s}\n' % layer
+        resultString += '\\pgfsetlayers{%s}\n' % layerList
+        return resultString
+
+
+    @staticmethod
+    def createPackagesString(packagesList):
+        resultString = ''
+        for options, package in packagesList:
+            resultString += '\usepackage[%s]{%s}\n' % (options, package)
+        return resultString
+
+
+    @staticmethod
+    def createTikzlibrariesString(librariesList):
+        resultString = ''
+        for library in librariesList.split(','):
+            resultString += '\usetikzlibrary{%s}\n' % library
+        return resultString
+
+
+class TexObject(object):
+    def __init__(self, texOptions):
+        self.texOptions = texOptions
+
+    def toString(self):
+        return self._composeString()
+    
+    def _composeString(self):
+        return ''
+    
+
+class TexMaster(TexObject):
+    def __init__(self,
+                 completeScope,
+                 fieldOfViewScope,
+                 overlayScope,
+                 texOptions
+                 ):
+        super(TexMaster, self).__init__(texOptions)
+        self.completeScope = completeScope
+        self.fieldOfViewScope = fieldOfViewScope
+        self.overlayScope = overlayScope
+
+    
+
+    def _composeString(self):
+        optionsString = self._composeOptions()
+        initialIndent = TexHelper.createIndentString(int(self.texOptions['indent']))
+        firstIndent = TexHelper.createIndentString(int(self.texOptions['indent']) + int(self.texOptions['indentStep']))
+        secondIndent = int(self.texOptions['indent']) + 2*int(self.texOptions['indentStep'])
+        composition = """%s\\resizebox{%s}{!}{
+%s\\begin{tikzpicture}%s
+%s
+%s
+%s
+%s\\end{tikzpicture}
+%s}""" % (initialIndent,
+          self.texOptions['scale'],
+          firstIndent,
+          optionsString,
+          TexHelper.indentEachLineInString(secondIndent, self.completeScope.toString()),
+          TexHelper.indentEachLineInString(secondIndent, self.fieldOfViewScope.toString()),
+          TexHelper.indentEachLineInString(secondIndent, self.overlayScope.toString()),
+          firstIndent,
+          initialIndent
+)
+        return composition
+
+
+    def _composeOptions(self):
+        return TexHelper.composeOptions(self.texOptions['tikzpicture'])
+
+
+class TexCompleteScope(TexObject):
+    def __init__(self, scopeOptions):
+        super(TexCompleteScope, self).__init__(scopeOptions)
+
+    def _composeString(self):
+        optionsString = TexHelper.composeOptions(self.texOptions['scope']['options'])
+        currIndent = 0
+        indentStep = int(self.texOptions['indentStep'])
+        composition = '\\begin{pgfonlayer}{%s}\n' % self.texOptions['layer']
+        currIndent += indentStep
+
+        composition += '%s\\begin{scope}%s\n' % (TexHelper.createIndentString(currIndent), optionsString)
+        
+        scopeString = ''
+        currIndent += indentStep
+        indentString = TexHelper.createIndentString(currIndent)
+
+        imageDict = self.texOptions['images']
+        imageKeys = [x[0] for x in sorted(imageDict.iteritems(), key=lambda y: int(y[1]['index']))]
+        imageDict[imageKeys[0]]['options']['anchor'] = 'south west'
+        if len(imageKeys) > 0:
+            scopeString += indentString + TexHelper.createImageNodeString(imageKeys[0],
+                                                                          imageDict[imageKeys[0]]['index'],
+                                                                          imageDict[imageKeys[0]]['options'],
+                                                                          self.texOptions['nodeNameBase']) + '\n'
+        for idx, key in enumerate(imageKeys):
+            if idx == 0:
+                continue
+            # imageDict[key]['options']['xshift'] =  
+            # '($(%s)-(%s)$)' % (imageDict[key]['nodeName'], imageDict[imageKeys[0]]['nodeName']) use this for later stuff
+            scopeString += indentString + \
+                TexHelper.createImageNodeString(key,
+                                                imageDict[key]['index'],
+                                                imageDict[key]['options'],
+                                                self.texOptions['nodeNameBase']) + \
+                 '\n'
+        composition += scopeString
+        currIndent -= indentStep
+
+        composition += '%s\\end{scope}\n' % (TexHelper.createIndentString(currIndent))
+
+        currIndent -= indentStep
+
+        composition += '\\end{pgfonlayer}'
+
+        return composition
+
+
+class TexFieldOfViewScope(TexObject):
+    def __init__(self, scopeOptions):
+        super(TexFieldOfViewScope, self).__init__(scopeOptions)
+
+    def _composeString(self):
+        # get fake scope first
+        scopeOptions = dict(self.texOptions['scope']['options'])
+        scopeOptions['opacity'] = '0.0'
+        scopeOptions['overlay'] = None
+        optionsString = TexHelper.composeOptions(scopeOptions)
+        currIndent = 0
+        indentStep = self.texOptions['indentStep']
+        composition = '\\begin{pgfonlayer}{%s}\n' % 'background'
+        currIndent += indentStep
+
+        composition += '%s\\begin{scope}%s\n' % (TexHelper.createIndentString(currIndent), optionsString)
+        currIndent += indentStep
+
+        scopeString = ''
+        indentString = TexHelper.createIndentString(currIndent)
+
+        imageDict = self.texOptions['images']
+        imageKeys = [x[0] for x in sorted(imageDict.iteritems(), key=lambda y: int(y[1]['index']))]
+
+        imageDict[imageKeys[0]]['options']['anchor'] = 'south west'
+        if len(imageKeys) > 0:
+            scopeString += indentString + TexHelper.createImageNodeString(imageKeys[0],
+                                                                          imageDict[imageKeys[0]]['index'],
+                                                                          imageDict[imageKeys[0]]['options'],
+                                                                          self.texOptions['fakeNameBase']) + '\n'
+        for idx, key in enumerate(imageKeys):
+            if idx == 0:
+                continue
+            scopeString += indentString + \
+              TexHelper.createImageNodeString(key,
+                                              imageDict[key]['index'],
+                                              imageDict[key]['options'],
+                                              self.texOptions['fakeNameBase']) + \
+              '\n'
+        composition += scopeString
+        currIndent -= indentStep
+        composition += '%s\\end{scope}\n'
+        
+        currIndent -= indentStep
+
+        composition += '\\end{pgfonlayer}'
+
+        # get real scope
+        scopeOptions = dict(self.texOptions['scope'])
+        optionsString = TexHelper.composeOptions(scopeOptions)
+
+        return composition
+
+
+class TexStandaloneDecorator(TexObject):
+    def __init__(self, texMaster, options):
+        super(TexStandaloneDecorator, self).__init__(options)
+        self.texMaster = texMaster
+
+    def _composeString(self):
+        res_string = '''\documentclass{standalone}
+%s
+%s
+%s
+\\begin{document}
+%s
+\\end{document}''' % (TexHelper.createPackagesString(self.texOptions['packages']),
+                     TexHelper.createTikzlibrariesString(self.texOptions['tikzlibraries']),
+                     TexHelper.createLayerString(self.texOptions['layers']),
+                     texMaster.toString())
+        return res_string
+        
+
+
+
+if __name__ == "__main__":
+    import copy
+    texDefaultOptions = FileComprehension.TexDefaultOptions()
+    userOptions = {}
+    parsedOptions = FileComprehension.OptionParser.parse(texDefaultOptions.mergeUserOptions(userOptions))
+    
+    # options = defaultdict(dict, {
+    #     'tikzpicture' :
+    #     {
+    #         'a' : 1, 'b' : None
+    #     },
+    #     'indent' : 0,
+    #     'indentStep' : 4,
+    #     'images' :
+    #     {
+    #         'a' :
+    #         {
+    #             'index' : 0, 'options' : {}, 'nodeName' : 'zig', 'nodeNameBase' : 'WUUUZA', 'fakeNameBase' : 'gefakt'
+    #         },
+    #         'b' :
+    #         {
+    #             'index':1, 'options' : {'xshift' : '50'}, 'nodeName' : 'mizzge', 'nodeNameBase' : 'BAAAAZE', 'fakeNameBase' : 'gefukt'
+    #         }
+    #     },
+    #     'xshift' : '10',
+    #     'scale' : 1.0,
+    #     'layer' : 'blalayer'})
+    texMaster = TexMaster(TexCompleteScope(parsedOptions['completeScope']),
+                          TexFieldOfViewScope(parsedOptions['fieldOfViewScope']),
+                          TexObject(parsedOptions['overlay']),
+                          parsedOptions['general']
+                          )
+    standalone = TexStandaloneDecorator(texMaster, parsedOptions['general'])
+
+    print standalone.toString()
